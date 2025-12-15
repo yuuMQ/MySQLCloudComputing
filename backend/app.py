c40e8ef70e922d49d52ff6d4f4cca46d47d85aff
yuuMQ <minhquan7ahaha@gmail.com>
2025-12-15 16:19:45 +0700

    update

--- backend/app.py
+++ backend/app.py
@@ -251,7 +251,7 @@ def add_row(dbname, table):
     cursor = conn.cursor()
 
     cursor.execute(f'DESCRIBE `{dbname}`.`{table}`;')
-    columns = [col[0] for col in cursor.fetchall() if col[0] != 'id']
+    columns = [col[0] for col in cursor.fetchall()]
 
     values = []
     for col in columns:
@@ -259,10 +259,13 @@ def add_row(dbname, table):
 
     cols_str = ','.join(f'`{c}`' for c in columns)
     placeholders = ','.join(['%s'] * len(values))
-
-    cursor.execute(f"INSERT INTO `{dbname}`.`{table}` ({cols_str}) VALUES ({placeholders})", values)
-    conn.commit()
-    conn.close()
+    try:
+        cursor.execute(f"INSERT INTO `{dbname}`.`{table}` ({cols_str}) VALUES ({placeholders})", values)
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        conn.rollback()
+        return redirect(url_for('view_table', dbname=dbname, table=table, err_msg=str(e)))
 
     return redirect(f'/database/{dbname}/{table}')
 
@@ -273,7 +276,7 @@ def edit_row(dbname, table, rowid):
     cursor = conn.cursor()
 
     cursor.execute(f"DESCRIBE `{dbname}`.`{table}`;")
-    columns = [col[0] for col in cursor.fetchall() if col[0] != "id"]
+    columns = [col[0] for col in cursor.fetchall()]
 
     assignments = []
     values = []
@@ -319,7 +322,7 @@ def create_database():
 
     try:
         create_user_db(username, db_name)
-        return redirect('/')
+        return redirect(url_for('index', msg="TẠO DATABASE THÀNH CÔNG"))
 
     except Exception as e:
         return f"Lỗi khi tạo database: {e}"
@@ -339,7 +342,7 @@ def drop_database(dbname):
 
         conn.commit()
         conn.close()
-        return redirect("/")
+        return redirect(url_for('index', msg="XÓA DATABASE THÀNH CÔNG"))
 
     except Exception as e:
         return f"Lỗi khi xóa database: {e}"
@@ -359,7 +362,7 @@ def create_table(dbname):
 
     sql = f"""
     CREATE TABLE `{dbname}`.`{table}` (
-        id INT AUTO_INCREMENT PRIMARY KEY
+        id INT
     );
     """
 
@@ -372,7 +375,7 @@ def create_table(dbname):
 
 
     conn.close()
-    return redirect(f'/database/{dbname}')
+    return redirect(url_for('list_tables',  dbname=dbname, msg="Thêm bảng thành công!!!"))
 
 # DROP TABLE
 @app.route('/database/<dbname>/drop_table/<table>')
@@ -390,8 +393,77 @@ def drop_table(dbname, table):
         return str(e)
 
     conn.close()
-    return redirect(f'/database/{dbname}')
+    return redirect(url_for('list_tables',  dbname=dbname, msg="Xóa bảng thành công!!!"))
 
+
+# ADD PRIMARY KEY
+@app.route('/database/<dbname>/<table>/add_primary_key', methods=['POST'])
+def add_primary_key(dbname, table):
+    # Lấy list các cột
+    columns = request.form.getlist("columns")
+
+    if not columns:
+        return redirect(f"/database/{dbname}/{table}?err_msg=Chưa chọn cột")
+
+    conn = user_conn(session['username'], session['user_pass'])
+    cursor = conn.cursor()
+
+    # 1. Check Primary Key
+    cursor.execute("""
+        SELECT COLUMN_NAME
+        FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+        WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s AND CONSTRAINT_NAME = 'PRIMARY'
+    """, (dbname, table))
+
+    if cursor.fetchone():
+        conn.close()
+        return redirect(f"/database/{dbname}/{table}?err_msg=Đã có PRIMARY KEY!!!")
+
+    # 2. MODIFY columns -> not null
+    for col in columns:
+        cursor.execute(f"ALTER TABLE {dbname}.{table} MODIFY `{col}` NVARCHAR(255) NOT NULL")
+
+    try:
+        # 3. ADD COMPOSITE PK
+        cols = ', '.join(f"`{c}`" for c in columns)
+        cursor.execute(f"ALTER TABLE {dbname}.{table} ADD PRIMARY KEY ({cols})")
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        conn.rollback()
+        return redirect(url_for('view_table', dbname=dbname, table=table, msg=str(e)))
+
+    return redirect(f"/database/{dbname}/{table}?msg=Thêm PRIMARY KEY thành công!!!!")
+
+# DROP PRIMARY KEY
+
+@app.route('/database/<dbname>/<table>/drop_primary_key', methods=['POST'])
+def drop_primary_key(dbname, table):
+    conn = user_conn(session['username'], session['user_pass'])
+    cursor = conn.cursor()
+
+    # 1. Check PK
+    cursor.execute("""
+            SELECT 1
+            FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+            WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s AND CONSTRAINT_NAME = 'PRIMARY'
+            LIMIT 1
+        """, (dbname, table))
+
+    if not cursor.fetchone():
+        conn.close()
+        return redirect(f"/database/{dbname}/{table}?err_msg=CHƯA CÓ PRIMARY KEY!!")
+
+    try:
+        # 2. Xóa PK
+        cursor.execute(f"ALTER TABLE {dbname}.{table} DROP PRIMARY KEY")
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        conn.close()
+        return redirect(url_for('view_table', dbname=dbname, table=table, msg=str(e)))
+    conn.close()
+    return redirect(f"/database/{dbname}/{table}?msg=XÓA PRIMARY KEY THÀNH CÔNG")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
